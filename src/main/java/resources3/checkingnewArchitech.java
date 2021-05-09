5d4e72df98b2cf9f14d320299e38ec3dcaf0d398
Nadeem412 <safiulnadeem@gmail.com>
2021-05-09 16:18:24 +0530

    added new class by architech

---
+++ src/main/java/resources3/checkingnewArchitech.java
@@ -0,0 +1,14 @@
+package resources3;
+
+import org.testng.annotations.Test;
+
+public class checkingnewArchitech {
+	
+@Test
+public void checkarchitec() {
+	
+	System.out.println("I am here in develope architech");
+	System.out.println("Where are they to develope the application");
+}
+
+}
